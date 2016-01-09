1f29fab950f6b30a2469cad2521716d5277a9c2d
kyawthetsein <kyawthetsein@gmail.com>
2016-01-09 07:30:35 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
   variable "AWS_ACCESS_KEY" {}
   variable "AWS_SECRET_KEY" {}
   variable "AWS_SSH_KEY" {}
+  variable "USER_PASSWORD" {}
   variable "ATLAS_TOKEN" {}
   variable "AWS_REGION" {
       default = "us-west-2"
